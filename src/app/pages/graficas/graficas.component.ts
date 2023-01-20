7390b2a22f9ab516256963c40a0b55a2fc0318a8
Marco Pineda <marcopnd88@gmail.com>
2023-01-20 01:25:12 -0500

    20/01/2023 administrador

---
+++ src/app/pages/graficas/graficas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas',
+  templateUrl: './graficas.component.html',
+  styles: [
+  ]
+})
+export class GraficasComponent {
+
+}
